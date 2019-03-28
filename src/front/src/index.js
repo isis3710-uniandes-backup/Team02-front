ab818737b200f60de78605b0a334149711346fce
ngaitan98 <n.gaitan@uniandes.edu.co>
2019-03-27 19:14:09 -0500

    Howdy

---
+++ src/front/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import LoginPage from './loginPage/loginPage.js'
+
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
+
---
+++ src/front/src/loginPage/loginPage.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './loginPage.css';
+import SignUp from '../signUpPage/signUp';
+import ReactDOM from 'react-dom';
+import MainPage from '../mainPage/mainPage';
+
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="d-flex justify-content-center">
+          <div className="card" id="carta">
+            <div className="card-header">
+              <h1><b>Sign In</b></h1>
+            </div>
+            <div className="card-body">
+              <div className="container-fluid row">
+                <div className="col-sm-12">
+                  <div className="d-flex justify-content-center option form-group">
+                  </div>
+                  <div className="d-flex justify-content-center option form-group">
+                    <button className="btn">
+                      <i className="fab fa-facebook-f"></i>
+                    </button>
+                    <button className="btn">
+                      <i className="fab fa-github"></i>
+                    </button>
+                    <button className="btn">
+                      <i className="fab fa-google-plus-g"></i>
+                    </button>
+                  </div>
+                  <div className="d-flex justify-content-center option form-group">
+                    <h4>or use email</h4>
+                  </div>
+                </div>
+              </div>
+              <form>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-user"></i></span>
+                  </div>
+                  <input type="email" className="form-control" placeholder="Email"></input>
+                </div>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-key"></i></span>
+                  </div>
+                  <input type="password" className="form-control" placeholder="password"></input>
+                </div>
+                <div className="form-group">
+                  <input type="submit" value="Login" className="btn float-right login_btn" onClick = {this.logUser}></input>
+                </div>
+              </form>
+              <br></br>
+              <br></br>
+              <div className="d-flex justify-content-center links">
+                Don't have an account? <span className="link" onClick={this.goToSignUp}> Sign Up</span>
+              </div>
+              <div className="d-flex justify-content-center links">
+                <span className="link"> Forgot your password?</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  goToSignUp() {
+    ReactDOM.render(<SignUp />, document.getElementById('root'));
+  }
+  logUser(){
+    ReactDOM.render(<MainPage />, document.getElementById('root'));
+  }
+}
+
+export default Login;
